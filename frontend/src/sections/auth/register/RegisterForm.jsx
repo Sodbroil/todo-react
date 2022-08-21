5fbe59e3ba3a80936df753275b68a12cc4bb6544
Sodbroil <nikd20222@gmail.com>
2022-08-22 00:33:02 +0300

    Добавлен вывод ошибок

--- frontend/src/sections/auth/register/RegisterForm.jsx
+++ frontend/src/sections/auth/register/RegisterForm.jsx
@@ -1,11 +1,9 @@
-import * as Yup from 'yup';
 import { useState } from 'react';
-import { useNavigate, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 // @mui
-import { Stack, IconButton, InputAdornment, TextField } from '@mui/material';
+import { Alert, IconButton, InputAdornment, Stack, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import Iconify from '../../../components/Iconify';
@@ -14,115 +12,94 @@ import { fetchRegister, selectIsAuth } from '../../../redux/slices/auth';
 // ----------------------------------------------------------------------
 
 export default function RegisterForm() {
-  const navigate = useNavigate();
+	const isAuth = useSelector(selectIsAuth);
+	const dispatch = useDispatch();
+	const [showPassword, setShowPassword] = useState(false);
+	const [hasError, setHasError] = useState(false);
 
-  // const [showPassword, setShowPassword] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: {errors, isValid},
+	} = useForm({
+		defaultValues: {
+			fullName: '',
+			email: '',
+			password: '',
+		},
+		mode: 'onChange',
+	});
 
-  // const RegisterSchema = Yup.object().shape({
-  //   firstName: Yup.string().required('First name required'),
-  //   lastName: Yup.string().required('Last name required'),
-  //   email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-  //   password: Yup.string().required('Password is required'),
-  // });
 
-  // const defaultValues = {
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   password: '',
-  // };
+	const onSubmit = async (values) => {
+		try {
+			const data = await dispatch(fetchRegister(values));
+			if ('token' in data.payload) {
+				window.localStorage.setItem('token', data.payload.token);
+			}
+		} catch (e) {
+			setHasError(true);
+		}
+	};
 
-  // const methods = useForm({
-  //   resolver: yupResolver(RegisterSchema),
-  //   defaultValues,
-  // });
+	function ErrorComponent() {
+		return <Alert severity="error" sx={{mb: 2}}>Ошибка регистрации. Обратитесь в поддержку.</Alert>
+	}
 
-  // const {
-  //   handleSubmit,
-  //   formState: { isSubmitting },
-  // } = methods;
+	if (isAuth) {
+		return <Navigate to="/app/todo"/>;
+	}
 
-  // const onSubmit = async () => {
-  //   navigate('/dashboard', { replace: true });
-  // };
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			{hasError && <ErrorComponent/>}
+			<Alert severity="info" sx={{mb: 2}}>Фамилия и Имя должны состоять как минимум из 3 символов </Alert>
+			<Alert severity="info" sx={{mb: 2}}>Пароль должен состоять минимум из 6 символов</Alert>
+			<Stack spacing={3}>
+				<TextField
+					name="fullName"
+					label="Фамилия Имя"
+					error={Boolean(errors.fullName?.message)}
+					helperText={errors.fullName?.message}
+					{...register('fullName', {required: 'Укажите Фамилию и Имя', minLength: 3})}
+				/>
 
-  const isAuth = useSelector(selectIsAuth);
-  const dispatch = useDispatch();
-  const [showPassword, setShowPassword] = useState(false);
+				<TextField
+					name="email"
+					label="Email address"
+					error={Boolean(errors.email?.message)}
+					helperText={errors.email?.message}
+					{...register('email', {
+						required: 'Укажите почту', pattern: {
+							value: /\S+@\S+\.\S+/,
+							message: "Введенное значение не соответствует формату электронной почты"
+						}
+					})}
+				/>
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isValid },
-  } = useForm({
-    defaultValues: {
-      fullName: '',
-      email: '',
-      password: '',
-    },
-    mode: 'onChange',
-  });
+				<TextField
+					name="password"
+					label="Пароль"
+					type={showPassword ? 'text' : 'password'}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<IconButton edge="end" onClick={() => setShowPassword(!showPassword)}>
+									<Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}/>
+								</IconButton>
+							</InputAdornment>
+						),
+					}}
+					error={Boolean(errors.password?.message)}
+					helperText={errors.password?.message}
+					{...register('password', {required: 'Укажите пароль', minLength: 6})}
+				/>
 
-  //  console.log(setError);
-
-  const onSubmit = async (values) => {
-    const data = await dispatch(fetchRegister(values));
-
-    if (!data.payload) {
-      return alert('Не удалось зарегистрироваться');
-    }
-
-    if ('token' in data.payload) {
-      window.localStorage.setItem('token', data.payload.token);
-    }
-  };
-
-  if (isAuth) {
-    return <Navigate to="/app/todo" />;
-  }
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={3}>
-        <TextField
-          name="fullName"
-          label="ФИО"
-          error={Boolean(errors.fullName?.message)}
-          helperText={errors.fullName?.message}
-          {...register('fullName', { required: 'Укажите ФИО' })}
-        />
-
-        <TextField
-          name="email"
-          label="Email address"
-          error={Boolean(errors.email?.message)}
-          helperText={errors.email?.message}
-          {...register('email', { required: 'Укажите почту' })}
-        />
-
-        <TextField
-          name="password"
-          label="Пароль"
-          type={showPassword ? 'text' : 'password'}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton edge="end" onClick={() => setShowPassword(!showPassword)}>
-                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-          error={Boolean(errors.password?.message)}
-          helperText={errors.password?.message}
-          {...register('password', { required: 'Укажите пароль' })}
-        />
-
-        <LoadingButton disabled={!isValid} fullWidth size="large" type="submit" variant="contained">
-          Зарегистрироваться
-        </LoadingButton>
-      </Stack>
-    </form>
-  );
+				<LoadingButton disabled={!isValid} fullWidth size="large" type="submit" variant="contained">
+					Зарегистрироваться
+				</LoadingButton>
+			</Stack>
+		</form>
+	);
 }
